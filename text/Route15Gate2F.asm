2e18d33099f2b431897f1274efbcbfff183aac41
cedricwaltercastroson <cedricwaltercastroson@hotmail.com>
2025-04-17 14:50:35 +1000

    Fixed small typo

--- text/Route15Gate2F.asm
+++ text/Route15Gate2F.asm
@@ -1,7 +1,7 @@
 _Route15Gate2FOaksAideExpAllText::
 	text "All the #MON"
 	line "in your party"
-	cont "receive exp points, "
+	cont "receive EXP points, "
 	cont "even if only"
 	cont "one fought."
 	done
